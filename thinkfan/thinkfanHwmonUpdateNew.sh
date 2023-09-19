3ccaf744cbc0c6466d2bd9841ea006d74eb4f00d
mschwarzmeier <89848589+mschwarzmeier@users.noreply.github.com>
2023-09-19 09:46:40 +0200

    Use one-digit number when replacing hwmon-number

--- thinkfan/thinkfanHwmonUpdateNew.sh
+++ thinkfan/thinkfanHwmonUpdateNew.sh
@@ -4,8 +4,8 @@
 THINKFAN_CONF="/etc/thinkfan.conf"
 
 
-# find the number of hwmon, e.g. "hwmon6"
-HWMONNUMBER=$(ls /sys/devices/platform/thinkpad_hwmon/hwmon)
+# find the number of hwmon, e.g. "hwmon6" -> 6
+HWMONNUMBER=$(ls /sys/devices/platform/thinkpad_hwmon/hwmon | grep -o '[0-9]')
 
-# replace "hwmon*" with the found HWMONNUMBER
-sed -i "s/\\/hwmon\\/hwmon./\\/hwmon\\/$HWMONNUMBER/g" "$THINKFAN_CONF"
+# replace with the found HWMONNUMBER
+sed -i "s/\\/hwmon\\/hwmon./\\/hwmon\\/hwmon$HWMONNUMBER/g" "$THINKFAN_CONF"
